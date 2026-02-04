d47b6b9223430c2e173fe59ea0050eb62750b202
Kajanan Yoganathan <kaja_nan@hotmail.de>
2026-02-04 23:32:44 +0100

    first commit

---
+++ auth_app/api/serializers.py
@@ -0,0 +1,43 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class RegistrationSerializer(serializers.Serializer):
+    fullname = serializers.CharField(max_length=150)
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True, min_length=8)
+    repeated_password = serializers.CharField(write_only=True, min_length=8)
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists.")
+        return value
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['repeated_password']:
+            raise serializers.ValidationError(
+                {"repeated_password": "Passwords do not match."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['email'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['fullname'],
+        )
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        user = authenticate(username=attrs['email'], password=attrs['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid email or password.")
+        attrs['user'] = user
+        return attrs
