9b780019f7ece255f0830cf0787cb83932d9ee44
Kajanan Yoganathan <kaja_nan@hotmail.de>
2026-02-10 15:35:56 +0100

    feat: register models in Django admin

--- board_app/admin.py
+++ board_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from board_app.models import Board
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "created_by", "created_at"]
+    search_fields = ["title"]
+    list_filter = ["created_at"]
--- tasks_app/admin.py
+++ tasks_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks_app.models import Comment, Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "status", "priority", "board", "assignee"]
+    search_fields = ["title"]
+    list_filter = ["status", "priority"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "author", "task", "created_at"]
+    list_filter = ["created_at"]
