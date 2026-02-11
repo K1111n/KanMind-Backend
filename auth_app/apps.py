1b2ef4a38f7eb8c11225cbcbe1d62072090f7aef
Kajanan Yoganathan <kaja_nan@hotmail.de>
2026-02-11 10:34:14 +0100

    fix: admin user interface changed

--- auth_app/apps.py
+++ auth_app/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class AuthAppConfig(AppConfig):
     name = 'auth_app'
+
+    def ready(self):
+        from django.contrib.auth import apps as auth_apps
+        auth_apps.AuthConfig.verbose_name = 'Users'
