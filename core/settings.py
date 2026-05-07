46a0fe15fcb05f7676a92062cff930e6420db47e
Kajanan Yoganathan <kaja_nan@hotmail.de>
2026-05-07 10:09:18 +0200

    fix: update CORS allowed origin to production URL

--- core/settings.py
+++ core/settings.py
@@ -139,5 +139,5 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5501',
     'http://127.0.0.1:5501',
-    'https://kanmind-frontend.onrender.com',
+    'https://project-kanmind.onrender.com',
 ]
